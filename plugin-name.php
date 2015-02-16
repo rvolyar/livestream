c606121f8a7088845410fc26768c3485127998c5
serialscoper101 <prizerebelcrap101@gmail.com>
2015-02-16 21:02:41 +0000

    changed plugin info

--- plugin-name.php
+++ plugin-name.php
@@ -10,18 +10,16 @@
  *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           Livestream_Check
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress dashboard.
+ * Plugin Name:       Livestream Checker
+ * Description:       Check if user is streaming.  If yes then display modal with the stream info.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Ruslan Volyar
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       Livestream_Check
  * Domain Path:       /languages
  */
 
